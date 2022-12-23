0062f20463d7fae166c5524c7d06549e1cfe4a75
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-12-23 04:43:05 +0000

    Connect app to database

---
+++ config/database.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
