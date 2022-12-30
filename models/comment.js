2f661de847f7cec3a9adb973f429ccc51a2e4929
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-12-30 14:45:04 +0000

    Add timestamps to post and comment models

---
+++ models/comment.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema(
+  {
+    author: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Comment needs to have an author"],
+    },
+    post: {
+      type: mongoose.Types.ObjectId,
+      ref: "Post",
+      required: [true, "Comment belongs to a post"],
+    },
+    text: String,
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Comment", CommentSchema);
--- models/post.js
+++ models/post.js
@@ -1,35 +1,38 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const PostSchema = new Schema({
-	author: {
-		type: mongoose.Types.ObjectId,
-		ref: "User",
-		required: [true, "Post needs to have an author"],
-	},
-	title: {
-		type: String,
-		required: [true, "Title is required"],
-	},
-	content: [
-		{
-			type: mongoose.Types.ObjectId,
-			ref: "Block",
-		},
-	],
-	comments: [
-		{
-			type: mongoose.Types.ObjectId,
-			ref: "Comment",
-		},
-	],
-	likes: [
-		{
-			type: mongoose.Types.ObjectId,
-			ref: "User",
-		},
-	],
-	published: Boolean,
-});
+const PostSchema = new Schema(
+  {
+    author: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: [true, "Post needs to have an author"],
+    },
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+    },
+    content: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Block",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+    likes: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    published: Boolean,
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Post", PostSchema);
