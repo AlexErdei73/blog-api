441b0581f0118c0119511148632a114187507afc
Alex Erdei <mralexerdei@yahoo.co.uk>
2022-12-31 12:45:40 +0000

    Add posts access points to api

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,5 +1,36 @@
 const Post = require("../models/post");
+const { body, validationResult } = require("express-validator");
 
 exports.posts_get = function (req, res, next) {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.posts_post = [
+  // TODO validation and sanitization,
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // Handle validation errors
+    }
+    res.send("NOT IMPLEMENTED");
+  },
+];
+
+exports.post_get = function (req, res, next) {
+  res.send("NOT IMPLEMENTED");
+};
+
+exports.post_put = [
+  // TODO validation and sanitization
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      // Handle validation errors
+    }
+    res.send("NOT IMPLEMETED");
+  },
+];
+
+exports.post_delete = function (req, res, next) {
   res.send("NOT IMPLEMENTED");
 };
--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,30 @@ var postsController = require("../controllers/postsController");
 /* GET posts listing */
 router.get("/", postsController.posts_get);
 
+/* GET posts/:id to get the existing post */
+router.get("/:id", postsController.post_get);
+
 module.exports = (passport) => {
+  /* POST posts to add new post to database */
+  router.post(
+    "/",
+    passport.authenticate("jwt", { session: false }),
+    postsController.posts_post
+  );
+
+  /* PUT posts/:id to update the existing post */
+  router.put(
+    "/:id",
+    passport.authenticate("jwt", { session: false }),
+    postsController.post_put
+  );
+
+  /* DELETE posts/:id to delete the post */
+  router.delete(
+    "/:id",
+    passport.authenticate("jwt", { session: false }),
+    postsController.post_delete
+  );
+
   return router;
 };
