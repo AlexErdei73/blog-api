8c96f28cea3d6169af95d170211c5c24210f5995
Alex Erdei <mralexerdei@yahoo.co.uk>
2023-03-18 13:00:27 +0000

    Add likes_put method to toggle like on a post

--- routes/posts.js
+++ routes/posts.js
@@ -14,27 +14,30 @@ router.get("/:id/blocks", postsController.blocks_get);
 /* GET posts/:id/comments to get the comments of the post */
 router.get("/:id/comments", postsController.comments_get);
 
+/* PUT posts/:id/likes to toggle like in the post likes */
+router.put("/:id/likes", postsController.likes_put);
+
 module.exports = (passport) => {
-  /* POST posts to add new post to database */
-  router.post(
-    "/",
-    passport.authenticate("jwt", { session: false }),
-    postsController.posts_post
-  );
+	/* POST posts to add new post to database */
+	router.post(
+		"/",
+		passport.authenticate("jwt", { session: false }),
+		postsController.posts_post
+	);
 
-  /* PUT posts/:id to update the existing post */
-  router.put(
-    "/:id",
-    passport.authenticate("jwt", { session: false }),
-    postsController.post_put
-  );
+	/* PUT posts/:id to update the existing post */
+	router.put(
+		"/:id",
+		passport.authenticate("jwt", { session: false }),
+		postsController.post_put
+	);
 
-  /* DELETE posts/:id to delete the post */
-  router.delete(
-    "/:id",
-    passport.authenticate("jwt", { session: false }),
-    postsController.post_delete
-  );
+	/* DELETE posts/:id to delete the post */
+	router.delete(
+		"/:id",
+		passport.authenticate("jwt", { session: false }),
+		postsController.post_delete
+	);
 
-  return router;
+	return router;
 };
